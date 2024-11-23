78c3e94e276fa5ee3dd8e000eca98738262f8892
josiah <josiah.chapman@gmail.com>
2024-11-22 16:02:19 -0800

    (~)

--- echo_runner/packages/echo_runner/main.py
+++ echo_runner/packages/echo_runner/main.py
@@ -1,4 +1,6 @@
 import os
+import traceback
+from time import sleep
 
 import requests
 from loguru import logger
@@ -21,18 +23,23 @@ def receive_echo(channel, method, properties, body):
             raise RuntimeError(f"bad status code when relaying echo: <{response.status_code}> {response.text}")
         logger.info(f"(*) relayed echo: {body}")
     except Exception as error:
-        logger.error(f"(!) echo failed with error: {error}")
+        logger.error(f"(!) echo failed with error: {error}\n{traceback.format_exc()}")
     finally:
         channel.basic_ack(delivery_tag=method.delivery_tag)
 
 
 def main():
-    with Rabbit(host, port, username, password) as rabbit:
-        rabbit.channel.basic_qos(prefetch_count=1)
-        rabbit.channel.queue_declare(queue="echo")
-        rabbit.channel.basic_consume(queue="echo", on_message_callback=receive_echo)
-        logger.info(f"setup complete, listening for echos")
-        rabbit.channel.start_consuming()
+    while True:
+        try:
+            with Rabbit(host, port, username, password) as rabbit:
+                rabbit.channel.basic_qos(prefetch_count=1)
+                rabbit.channel.queue_declare(queue="echo")
+                rabbit.channel.basic_consume(queue="echo", on_message_callback=receive_echo)
+                logger.info(f"setup complete, listening for echos")
+                rabbit.channel.start_consuming()
+        except Exception as error:
+            logger.error(f"(!) rabbit connection failed with error: {error}\n{traceback.format_exc()}")
+            sleep(3)
 
 
 if __name__ == '__main__':
